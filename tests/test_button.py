b911150b3290a1048ade2b9c3a869c15acead678
Eugene Okulik <tiburon854@gmail.com>
2023-05-24 19:14:52 +0300

    first action

--- tests/test_button.py
+++ tests/test_button.py
@@ -1,11 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 import pytest
 
 
 @pytest.fixture()
 def browser():
-    chrome_browser = webdriver.Chrome()
+    options = Options()
+    options.add_argument('--headless')
+    chrome_browser = webdriver.Chrome(options=options)
     return chrome_browser
 
 
