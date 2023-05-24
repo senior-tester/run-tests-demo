bc422191bc611aed2ed674548cc0721a52075686
Eugene Okulik <tiburon854@gmail.com>
2023-05-24 19:36:08 +0300

    workflow dispatch

--- tests/test_button.py
+++ tests/test_button.py
@@ -15,3 +15,8 @@ def browser():
 def test_button_exist(browser):
     browser.get('https://www.qa-practice.com/elements/button/simple')
     assert browser.find_element(By.ID, 'submit-id-submit').is_displayed()
+
+
+def test_button_exist_2(browser):
+    browser.get('https://www.qa-practice.com/elements/button/like_a_button')
+    assert browser.find_element(By.PARTIAL_LINK_TEXT, 'Click').is_displayed()
